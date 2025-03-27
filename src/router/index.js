3abf1cc2ff4f59cecef5ba836e6d66284a6d88df
healzy <134807658+Healzy1@users.noreply.github.com>
2025-03-27 17:47:45 -0400

    fix: ajustes de configuração para deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router"; // modificado para hash history teste
 
 const routes = [
   {
@@ -23,7 +23,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // modificado para hash history teste
   routes,
   scrollBehavior() {
     return { top: 0, behavior: "smooth" };
